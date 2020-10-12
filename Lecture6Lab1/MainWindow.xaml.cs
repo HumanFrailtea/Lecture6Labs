612a25d33e2715594cdcf96c578969d09f354748
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-10-12 16:47:27 -0700

    Add project files.

---
+++ Lecture6Lab1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture6Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                int operation = operationBox.SelectedIndex;
+                decimal leftHand = Convert.ToDecimal(leftBox.Text);
+                decimal rightHand = Convert.ToDecimal(rightBox.Text);
+                decimal result = 0;
+
+                switch(operation)
+                {
+                    case 0:
+                        result = leftHand + rightHand;
+                        resultBox.Text = result.ToString();
+                        break;
+                    case 1:
+                        result = leftHand - rightHand;
+                        resultBox.Text = result.ToString();
+                        break;
+                    case 2:
+                        result = leftHand * rightHand;
+                        resultBox.Text = result.ToString();
+                        break;
+                    case 3:
+                        result = leftHand / rightHand;
+                        resultBox.Text = result.ToString();
+                        break;
+                    case 4:
+                        result = leftHand % rightHand;
+                        resultBox.Text = result.ToString();
+                        break;
+                    default:
+                        result = 0;
+                        MessageBox.Show("No operation selected.");
+                        break;
+
+                }
+            }
+            catch (DivideByZeroException )
+                    {
+                MessageBox.Show("Divide by Zero.");
+
+            }
+        }
+        }
+    }
+
---
+++ Lecture6Lab2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Lecture6Lab2
+{
+
+	public class Fraction
+	{
+		private int numerator;
+		private int denominator;
+
+		public Fraction()
+		{
+			numerator = 0;
+			denominator = 1;
+		}
+
+		public Fraction(int newNumerator)
+		{
+			numerator = newNumerator;
+			denominator = 1;
+		}
+
+		public Fraction(int newNumerator, int newDenominator)
+		{
+			Numerator = newNumerator;
+			Denominator = newDenominator;
+		}
+
+		public virtual int Numerator
+		{
+			set
+			{
+				numerator = value;
+			}
+			get
+			{
+				return numerator;
+			}
+		}
+
+		public virtual int Denominator
+		{
+			set
+			{
+				if (value != 0)
+				{
+					denominator = value;
+				}
+				else
+				{
+					denominator = 1;
+				}
+			}
+			get
+			{
+				return denominator;
+			}
+		}
+
+
+
+		public virtual double FractionValue
+		{
+			get
+			{
+				return (double)numerator / denominator;
+			}
+		}
+
+		public override string ToString()
+		{
+			reduceFraction();
+			return numerator + "/" + denominator;
+		}
+
+		public virtual void printFraction()
+		{
+			reduceFraction();
+			Console.WriteLine(numerator + "/" + denominator);
+		}
+
+		private void reduceFraction()
+		{
+			int n = numerator;
+			int d = denominator;
+
+			while (n != 0)
+			{
+				int temp = n;
+				n = d % n;
+				d = temp;
+			}
+
+			numerator = numerator / d;
+			denominator = denominator / d;
+		}
+	}
+
+}
